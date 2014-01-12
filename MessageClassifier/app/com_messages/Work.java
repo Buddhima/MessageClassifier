79d8cbc313207dcd1290c3084c3d009d1e00d8ec
prabhath <sumindappp@yahoo.com>
2014-01-12 11:27:23 +0530

    Changes in work class

--- MessageClassifier/app/com_messages/Work.java
+++ MessageClassifier/app/com_messages/Work.java
@@ -1,5 +1,7 @@
 package com_messages;
 
+import models.com.mc.messages.TextMessage;
+
 import java.io.Serializable;
 
 /**
@@ -8,89 +10,25 @@ import java.io.Serializable;
  */
 public class Work implements Serializable {
 
-    /**
-     * Original Message
-     */
-    private String job;
 
-    /**
-     * Classification Results
-     */
-    private String context;
-    private String gender;
-    private String language;
-    private String spam;
     private String workId;
+    private TextMessage message;
 
     public Work(String message, String workId) {
-        this.job = message;
+        this.message=new TextMessage(message);
         this.workId = workId;
 
     }
 
     public String getJob() {
-        return job;
+        return message.getMessage();
     }
 
     public String getWorkId() {
         return workId;
     }
 
-    /**
-     * @return the context
-     */
-    public String getContext() {
-        return context;
-    }
 
-    /**
-     * @param context the context to set
-     */
-    public void setContext(String context) {
-        this.context = context;
-    }
-
-    /**
-     * @return the gender
-     */
-    public String getGender() {
-        return gender;
-    }
-
-    /**
-     * @param gender the gender to set
-     */
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    /**
-     * @return the language
-     */
-    public String getLanguage() {
-        return language;
-    }
-
-    /**
-     * @param language the language to set
-     */
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
-    /**
-     * @return the spam
-     */
-    public String getSpam() {
-        return spam;
-    }
-
-    /**
-     * @param spam the spam to set
-     */
-    public void setSpam(String spam) {
-        this.spam = spam;
-    }
 
 
 }
