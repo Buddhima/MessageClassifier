509d2d696c34fc4e23cd90a76cdb6b8c1ef4baf4
prabhath <sumindappp@yahoo.com>
2014-01-16 18:05:49 +0530

    Ui improvements

--- MessageClassifier/app/models/com/mc/workers/WorkResultConsumer.java
+++ MessageClassifier/app/models/com/mc/workers/WorkResultConsumer.java
@@ -6,32 +6,48 @@ import akka.contrib.pattern.DistributedPubSubExtension;
 import akka.contrib.pattern.DistributedPubSubMediator;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
+import models.com.mc.messages.TextMessage;
+import models.com.mc.messages.store.ObjectDBMsgStore;
 import models.com.mc.workers.Master.WorkResult;
 
-/**
- * @deprecated to be removed
- * WorkResult Consuming is done at MessageCollectingActor
- */
 public class WorkResultConsumer extends UntypedActor {
 
-  private ActorRef mediator = DistributedPubSubExtension.get(getContext().system()).mediator();
-  private LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+    ObjectDBMsgStore odbMsgStore;
 
-  {
-    mediator.tell(new DistributedPubSubMediator.Subscribe(Master.ResultsTopic, getSelf()), getSelf());
-  }
+    /* (non-Javadoc)
+     * @see akka.actor.UntypedActor#preStart()
+     */
+    @Override
+    public void preStart() throws Exception {
+        super.preStart();
 
-  @Override
-  public void onReceive(Object message) {
-    if (message instanceof DistributedPubSubMediator.SubscribeAck) {
-      // do nothing
+        // Set ObjectDBMsgStore instance
+        odbMsgStore = new ObjectDBMsgStore();
     }
-    else if (message instanceof WorkResult) {
-      WorkResult workResult = (WorkResult) message;
-      log.info("Consumed result: {}", workResult.result);
+
+    private ActorRef mediator = DistributedPubSubExtension.get(getContext().system()).mediator();
+    private LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+
+    {
+        mediator.tell(new DistributedPubSubMediator.Subscribe(Master.ResultsTopic, getSelf()), getSelf());
     }
-    else {
-      unhandled(message);
+
+
+
+
+    @Override
+    public void onReceive(Object message) {
+        if (message instanceof DistributedPubSubMediator.SubscribeAck) {
+            // do nothing
+        }
+        else if (message instanceof WorkResult) {
+            WorkResult workResult = (WorkResult) message;
+            log.info("Consumed result: {}", workResult.result);
+            //save messages
+            odbMsgStore.offer((TextMessage) workResult.getResult());
+        }
+        else {
+            unhandled(message);
+        }
     }
-  }
 }
